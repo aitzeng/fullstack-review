ef2670b6152d20c1c93ae887059b24105866eaf6
aitzeng <aitzeng@gmail.com>
2023-04-24 22:58:48 -0700

    Updated to add ajax

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,10 +6,25 @@ import RepoList from './components/RepoList.jsx';
 
 const App = () => {
 
+  // Top 25 repos in the database. Will need to use useEffect for when opening site
   const [repos, setRepos] = useState([]);
 
+  let server = 'http://localhost:1128/repos'
+
   const search = (term) => {
-    console.log(`${term} was searched`);
+    $.ajax({
+      url: server,
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({term}),
+      success: (response) => {
+        console.log('Successfully posted:', response)
+      },
+      error: (error) => {
+        console.log('Failed to post:', error)
+      }
+    })
+    // console.log(`${term} was searched`);
   }
 
   return (
