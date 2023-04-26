5c9e0eacb5c44ca89cb9410b71deb14c08a32762
aitzeng <aitzeng@gmail.com>
2023-04-25 17:32:32 -0700

    updated

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 let app = express();
 const { getReposByUsername } = require('../helpers/github.js');
+const { save, getTop } = require('../database/index.js');
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
@@ -13,9 +14,35 @@ app.use(morgan('dev'));
 
 app.post('/repos', function (req, res) {
   let requestedUsername = req.body.term;
-  console.log('THIS IS REQ.BODY:', req.body)
   getReposByUsername(requestedUsername)
-  .then()
+  .then((repos) => {
+    let currentData = repos.data;
+    let databaseResponses = currentData.map((repo) => {
+      let params = {_id: repo.id, name: repo.name, owner: repo.owner.login, createdAt: repo.created_at, updatedAt: repo.updated_at, html_url: repo.html_url, description: repo.description}
+      return save(params)
+    })
+    return Promise.all(databaseResponses)
+  })
+  .then((dbres) => {
+    res.send()
+  })
+  .catch((error) => {
+    res.send(error);
+  })
+  // .then((repos) => {
+  //   let currentData = repos.data;
+  //   for (var i = 0; i < currentData.length; i++) {
+  //     let params = {repoid: repo.id, name: repo.name, owner: repo.owner.login, createdAt: repo.created_at, updatedAt: repo.updated_at, html_url: repo.html_url, description: repo.description}
+  //     save(params)
+  //     .then((result) => {
+  //       console.log('This is result added to database:', result)
+  //     })
+  //     .catch((err) => {
+  //       console.log('Did not add to database:', err)
+  //     })
+  //   }
+  // })
+  // Model.create();
 
   // TODO - your code here!
   // This route should take the github username provided
@@ -24,6 +51,14 @@ app.post('/repos', function (req, res) {
 });
 
 app.get('/repos', function (req, res) {
+  return getTop()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    res.send(500, 'Error retrieving')
+  })
+
   // TODO - your code here!
   // This route should send back the top 25 repos
 });
